7e2535d1ba14e6b0c416b147240f9151c0295526
Dillon Burns <reach.dillon@gmail.com>
2017-03-22 12:59:14 -0400

    styling and added webfonts

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-$(document).ready(function(e) {
+$(document).ready(function (e) {
 
   var docHeight = window.innerHeight;
-  $(document).scroll(function(e) {
+  var docWidth = window.innerWidth;
+  $(document).scroll(function (e) {
     var scrollPos = $(document).scrollTop();
     if (scrollPos >= 250) {
       $(".nav").addClass("sticky");
@@ -12,8 +13,22 @@ $(document).ready(function(e) {
       $(".nav").removeClass("sticky");
       $("video").removeClass("sticky");
     }
+
+    /*var movementStrength = 10;
+    var height = movementStrength / docHeight;
+    var width = movementStrength / docWidth;
+    $(".content__img").mousemove(function (e) {
+      var pageX = e.pageX - ($(window).width() / 2);
+      var pageY = e.pageY - ($(window).height() / 2);
+      var newvalueX = width * pageX * -1 - 25;
+      var newvalueY = height * pageY * -1 - 50;
+      $(this).css("background-position", newvalueX + "px     " + newvalueY + "px");
+    });*/
+
+
   });
-  $(".scrollMore").click(function(e) {
+
+  $(".scrollMore").click(function (e) {
     scrollToStart();
   });
 
