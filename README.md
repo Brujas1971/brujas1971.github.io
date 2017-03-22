9aac8472433fdab55aff4dc13bb299f5464cffa9
Dillon Burns <reach.dillon@gmail.com>
2017-03-22 16:59:28 -0400

    mobile styling edits & formatting project;

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - to run locally run `http-server` in the project directory
 - to compile sass run `sass --watch styles/styles.scss:styles/styles.css`
+
+## The Battle Of The Build Systemz
+
+- to install dev env `npm install`
+- to watch sass & pug `gulp watch`
--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,9 @@ var docHeight = window.innerHeight;
 var docWidth = window.innerWidth;
 
 $(document).ready(function (e) {
-
+  
+  $(document).scrollTop(0);
+  
   $(document).scroll(function (e) {
     var scrollPos = $(document).scrollTop();
     if (scrollPos >= 250) {
@@ -16,16 +18,22 @@ $(document).ready(function (e) {
     }
   });
   $(".scrollMore").click(function (e) {
-    scrollToStart();
+    scrollToStart(1500);
+  });
+  $(".logo").click(function (e) {
+    scrollToStart(1500);
   });
 
-  function scrollToStart() {
+  function scrollToStart(time) {
     $("html, body").animate({
       scrollTop: docHeight * 1.85
-    }, 1500);
+    }, time);
   }
 });
 /*
+//  Programatically sets height & width of image parent container
+//  Apparently is not necessary
+
 $(window).on("load", function () {
   
   $(".tilt-effect").each(function (e) {
